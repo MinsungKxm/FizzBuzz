e809ec8345960c53480b1a10e7c072ebb5edb783
Minsung Kim <minsungk101@gmail.com>
2026-09-15 14:18:01 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -6,10 +6,8 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 100; i++) {
-
-            // Find out which numbers divide i.
+        int i = 1;
+        while (i <= 100){
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
@@ -31,6 +29,8 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
+
     }
 }
